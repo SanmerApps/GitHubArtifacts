2e4fb87d5c11b964a3747d9e21d7f04863ffed60
SanmerDev <sanmer@sanmer.dev>
2024-08-06 15:44:17 +0800

    Bump version to 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 
-val baseVersionName = "0.0.1"
+val baseVersionName = "0.1.0"
 val devVersion = exec("git tag --contains HEAD").isEmpty()
 val commitShaSuffix = commitSha.let { ".${it.substring(0, 7)}" }
 val devSuffix = if (devVersion) ".dev" else ""
