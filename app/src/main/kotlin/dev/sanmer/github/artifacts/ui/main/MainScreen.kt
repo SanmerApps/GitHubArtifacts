ac3b6c6b2ab94f04129a044eb090066b81d1626a
SanmerDev <sanmer@sanmer.dev>
2025-04-22 16:05:08 +0800

    Add background for `NavHost`

--- app/src/main/kotlin/dev/sanmer/github/artifacts/ui/main/MainScreen.kt
+++ app/src/main/kotlin/dev/sanmer/github/artifacts/ui/main/MainScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
@@ -29,6 +32,9 @@ fun MainScreen() {
     val navController = rememberNavController()
 
     NavHost(
+        modifier = Modifier.background(
+            color = MaterialTheme.colorScheme.background
+        ),
         navController = navController,
         startDestination = Screen.Home()
     ) {
